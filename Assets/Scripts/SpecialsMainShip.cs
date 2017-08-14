e82a95c215e2f6aa820593ba8a4ce86f68d56369
Thomas Pommerening <pommermann@hotmail.de>
2017-08-14 19:23:15 +0200

    Added Prototype of Loadinganimation for SpezialButton 1

--- Assets/Scripts/SpecialsMainShip.cs
+++ Assets/Scripts/SpecialsMainShip.cs
@@ -5,7 +5,7 @@ using UnityEngine;
 public class SpecialsMainShip : MonoBehaviour {
 
     GameObject[] allyArray;
-    float shieldDuration = 2f;
+    float shieldDuration = 5f;
 	// Use this for initialization
 	void Start () {
 		
---
+++ Assets/Scripts/UI/ButtonBehaviour.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ButtonBehaviour : MonoBehaviour {
+
+    public Transform loadingCircle;
+    public Transform textIndicator;
+    public Transform textLoading;
+
+    [SerializeField] private float currentAmount;
+    [SerializeField] private float speed;
+    // Use this for initialization
+
+    void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		if(currentAmount < 100)
+        {
+            currentAmount += speed * Time.deltaTime;
+            textIndicator.GetComponent<Text>().text = ((int)currentAmount).ToString() + "%";
+            textLoading.gameObject.SetActive(true);
+        }
+        else
+        {
+            textLoading.gameObject.SetActive(false);
+            textIndicator.GetComponent<Text>().text = "DONE!";
+        }
+        loadingCircle.GetComponent<Image>().fillAmount = currentAmount / 100;
+	}
+    void CooldownButton()
+    {
+
+    }
+}
