fe54c6ed0f2c93efe3b893f76e408c07be630d57
Thomas Pommerening <pommermann@hotmail.de>
2017-10-03 17:50:37 +0200

    Added StartScene Loading on StartButton + AudioManager

---
+++ Assets/Scripts/UI/MainMenu/SoundOption.cs
@@ -0,0 +1,42 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class SoundOption : MonoBehaviour {
+
+    bool AudioOn = true;
+    GameObject SoundOn;
+    GameObject SoundOff;
+
+	// Use this for initialization
+	void Start () {
+        SoundOff = GameObject.Find("SoundOff");
+        SoundOn = GameObject.Find("SoundOn");
+        SoundOff.SetActive(false);
+    }
+	
+	// Update is called once per frame
+	void Update () {
+		
+	}
+    public void SwitchSound()
+    {
+        if (AudioOn)
+        {
+            AudioListener.volume = 0;
+            AudioListener.pause = true;
+            SoundOn.SetActive(false);
+            SoundOff.SetActive(true);
+        }
+        else
+        {
+            AudioListener.volume = 1;
+            AudioListener.pause = false;
+            SoundOff.SetActive(false);
+            SoundOn.SetActive(true);
+        }
+        AudioOn = !AudioOn;
+
+    }
+}
