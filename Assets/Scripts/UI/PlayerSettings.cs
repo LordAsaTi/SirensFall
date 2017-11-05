a0b3015d20029e5439cf678fbd83e1f5c3bde7d6
Thomas Pommerening <pommermann@hotmail.de>
2017-11-05 19:05:00 +0100

    First Testbuild for SpezialsSettings not working

---
+++ Assets/Scripts/UI/PlayerSettings.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class PlayerSettings : MonoBehaviour {
+
+    public GameObject OptionsMenu;
+
+    public GameObject spezial1Zone;
+    public GameObject spezial2Zone;
+    public GameObject buttonPref;
+    public GameObject choiceContentPanel;
+    public string[] spezialsName;
+
+    void AddChoice(string choiceName, UnityEngine.Events.UnityAction method)
+    {
+        GameObject button = Instantiate(buttonPref, choiceContentPanel.transform);
+        button.GetComponent<ButtonPrefabScript>().label.text = choiceName;
+        button.GetComponent<Button>().onClick.AddListener(method);
+    }
+    public void CreateChoices()
+    {
+        for(int i = 0; i < spezialsName.Length; i++)
+        {
+            AddChoice(spezialsName[i], ButtonAktion);
+        }
+        
+    }
+    void ButtonAktion()
+    {
+        Debug.Log("Peng");
+    }
+    public void Activate()
+    {
+        OptionsMenu.SetActive(true);
+        CreateChoices();
+    }
+}
