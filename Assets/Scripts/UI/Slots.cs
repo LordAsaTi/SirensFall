ef482af798425e8fc3ece29a4ff2152bbec15381
Thomas Pommerening <pommermann@hotmail.de>
2017-11-05 20:08:30 +0100

    Added Shitty and Buggy Drag and Drop system

---
+++ Assets/Scripts/UI/Slots.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.EventSystems;
+
+public class Slots : MonoBehaviour , IDropHandler
+{
+    public GameObject item {
+        get {
+            if (transform.childCount > 0) {
+                return transform.GetChild(0).gameObject;
+            }
+            return null;
+        }
+    }
+    #region IdropHandler implementation 
+    public void OnDrop(PointerEventData eventData)
+    {
+       if (!item)
+        {
+            DragHandling.item.transform.SetParent(transform);
+        }
+    }
+    #endregion
+}﻿
