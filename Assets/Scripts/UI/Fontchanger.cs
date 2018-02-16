a14e973fd79aaea15dc73ecb10df5ea5bdf096a9
Thomas Pommerening <pommermann@hotmail.de>
2018-02-16 16:40:27 +0100

    Minor Bugfixes + changed color of loading circle

---
+++ Assets/Scripts/UI/Fontchanger.cs
@@ -0,0 +1,15 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class Fontchanger : MonoBehaviour {
+
+	// Use this for initialization
+	void Start () {
+        var textComponents = Component.FindObjectsOfType<Text>();
+        foreach (var component in textComponents)
+            component.font = ;
+
+    }
+}
