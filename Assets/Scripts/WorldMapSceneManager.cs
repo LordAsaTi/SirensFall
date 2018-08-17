03e0f3411f6ec85b6ab559158652787248cfcc70
Thomas Pommerening <pommermann@hotmail.de>
2018-08-18 00:14:02 +0200

    MInor Fixes

--- Assets/Scripts/WorldMapSceneManager.cs
+++ Assets/Scripts/WorldMapSceneManager.cs
@@ -1,16 +1,40 @@
 ﻿using System.Collections;
-using System.Collections.Generic;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.UI;
 
 public class WorldMapSceneManager : MonoBehaviour {
 
+    public GameObject LoadingScreen;
+    public Slider slider;
+
 	public void LoadLevel(int sceneNumber)
     {
-        SceneManager.LoadScene(sceneNumber);
+        if(LoadingScreen != null)
+        {
+            StartCoroutine(LoadAsynchronously(sceneNumber));
+        }
+        else
+        {
+            SceneManager.LoadScene(sceneNumber);
+        }
     }
     public void LoadLevel(string sceneName)
     {
         SceneManager.LoadScene(sceneName);
+    }
+
+    IEnumerator LoadAsynchronously (int sceneIndex)
+    {
+        AsyncOperation operation = SceneManager.LoadSceneAsync(sceneIndex);
+        LoadingScreen.SetActive(true);
+
+        while (!operation.isDone)
+        {
+            float progress = Mathf.Clamp01(operation.progress / .9f);
+
+            slider.value = progress;
+            yield return null;
+        }
     }
 }
